2d98d706dc84e0679af5c526cd7df59f00c130cb
Xuliang Mei <xuliang.mei@airwallex.com>
2026-03-09 00:22:21 +0800

    Remove /lm_blog base path for root domain deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://LyonMay.github.io',
-	base: '/lm_blog',
 	integrations: [mdx(), sitemap()],
 });
